2810155a39f542aa49ed05f3631b6dc4eedbf49c
zhonghaoqing <zhonghaoqing@zhonghaoqingtekiMacBook-Pro.local>
2012-09-13 14:47:08 +0800

    Initial Commit

---
+++ HKAlertView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HKAlertView
+//
+//  Created by zhonghaoqing on 12-9-13.
+//  Copyright (c) 2012年 waterhk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
